644f93d5567efadfd459ccc820940f78415ebbed
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-04-06 21:09:22 +0800

    add insert and removeAt

--- LinkedList/index.js
+++ LinkedList/index.js
@@ -18,6 +18,52 @@ class LinkedList {
 
     this.size++;
   }
+  insert(index, item) {
+    if (index > -1 && index <= this.size) {
+      let newNode = new node(item);
+      let current = this.head;
+      if (index === 0) {
+        newNode.next = current;
+        this.head = newNode;
+      } else {
+        let previous = null;
+        //從頭開始找到要insert的Index
+        let listIdx = 0;
+        while (listIdx != index) {
+          listIdx++;
+          previous = current;
+          current = current.next;
+        }
+        newNode.next = current;
+        previous.next = newNode;
+        this.size++;
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+  removeAt(index) {
+    let current = this.head;
+    if (index > -1 && index < this.size) {
+      if (index === 0) {
+        this.head = this.head.next;
+      } else {
+        let previous;
+        let listIdx = 0;
+        while (listIdx != index) {
+          listIdx++;
+          previous = current;
+          current = current.next;
+        }
+        //找到要刪除的index, 把上一個next指向現在的next
+        previous.next = current.next;
+        this.size--;
+      }
+    } else {
+      return false; //無法執行
+    }
+  }
 }
 
 class node {
@@ -31,4 +77,6 @@ const List = new LinkedList();
 List.append(1);
 List.append(2);
 List.append(3);
+List.insert(1, 6);
+List.removeAt(1);
 console.log(List);
