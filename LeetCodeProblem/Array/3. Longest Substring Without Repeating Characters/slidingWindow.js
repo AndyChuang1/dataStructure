317a69a6eb7312912089138ba6b8d696968fc7b9
Andychuang <bboy81905@gmail.com>
2020-06-17 10:07:07 +0800

    3. Longest Substring Without Repeating Characters

---
+++ LeetCodeProblem/Array/3. Longest Substring Without Repeating Characters/slidingWindow.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let r = 0,
+    l = 0;
+  let set = new Set();
+  let longLength = 0;
+
+  while (r < s.length) {
+    if (!set.has(s[r])) {
+      set.add(s[r]);
+      longLength = Math.max(longLength, r - l + 1);
+      r++;
+    } else {
+      set.delete(s[l]);
+      l++;
+    }
+  }
+  return longLength;
+};
