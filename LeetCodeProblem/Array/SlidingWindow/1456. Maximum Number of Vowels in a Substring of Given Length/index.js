fe34dc5b20c477172892eb1bb1a5468342c0c5a4
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-06-26 17:22:20 +0800

    add annonation

--- LeetCodeProblem/Array/SlidingWindow/1456. Maximum Number of Vowels in a Substring of Given Length/index.js
+++ LeetCodeProblem/Array/SlidingWindow/1456. Maximum Number of Vowels in a Substring of Given Length/index.js
@@ -30,12 +30,15 @@ var maxVowels2 = function (s, k) {
 
   for (let r = 0; r < s.length; r++) {
     if (vSet.has(s[r])) vowelNum++;
+    //把window 限制成為k，如果window >k 就要移windowStart
     while (r - l + 1 > k) {
+      //如果要刪掉的字元是母音 計數的vowelNum 需要扣除掉
       if (vSet.has(s[l])) {
         vowelNum--;
       }
       l++;
     }
+    // 只要最大值
     max = Math.max(vowelNum, max);
   }
   return max;
