71a846566537ba67c4d2b11dae91c0fb5425ae85
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-06-26 14:09:02 +0800

    review Max Consecutive Ones III

--- LeetCodeProblem/Array/SlidingWindow/1004. Max Consecutive Ones III/index.js
+++ LeetCodeProblem/Array/SlidingWindow/1004. Max Consecutive Ones III/index.js
@@ -20,3 +20,25 @@ var longestOnes = function (A, K) {
 
 longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2);
 // longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1], 0);
+
+//這比較好懂，但效率差一點
+var longestOnes = function (A, K) {
+  let l = 0;
+  let changeNum = 0;
+  let ans = 0;
+
+  for (let r = 0; r < A.length; r++) {
+    //記錄用過K的次數
+    if (A[r] === 0) changeNum++;
+    //超過K時 l開始移動，遇到0代表之前有改過，就把他去掉
+    while (changeNum > K) {
+      if (A[l] === 0) {
+        changeNum--;
+      }
+      l++;
+    }
+    ans = Math.max(ans, r - l + 1);
+  }
+
+  return ans;
+};
