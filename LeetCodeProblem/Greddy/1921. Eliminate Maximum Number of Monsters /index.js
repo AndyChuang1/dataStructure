0da6236014175a10e4587a09b0be286be9ea5a6d
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2021-09-12 13:28:46 +0800

    1921. Eliminate Maximum Number of Monsters

---
+++ LeetCodeProblem/Greddy/1921. Eliminate Maximum Number of Monsters /index.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} dist
+ * @param {number[]} speed
+ * @return {number}
+ */
+var eliminateMaximum = function (dist, speed) {
+  let killCount = 0;
+
+  const timeWithIdx = [];
+
+  for (let i = 0; i < dist.length; i++) {
+    const time = dist[i] / speed[i];
+    timeWithIdx.push([i, time]);
+  }
+
+  timeWithIdx.sort((a, b) => {
+    return a[1] - b[1];
+  });
+
+  for (let i = 0; i < timeWithIdx.length; ++i) {
+    const curE = dist[timeWithIdx[i][0]] - speed[timeWithIdx[i][0]] * i;
+    if (curE <= 0 && i !== 0) break;
+    killCount++;
+  }
+  console.log(killCount);
+  return killCount;
+};
+// eliminateMaximum([1, 1, 2, 3], [1, 1, 1, 1]);
+
+eliminateMaximum([4, 8, 6, 8, 2, 7, 4], [1, 3, 3, 1, 10, 1, 1]);
